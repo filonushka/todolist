7e9de08f6681a6dcd2b1db20750c1f5e3cfbca0d
filonushka <lero4ka.filonova@mail.ru>
2022-09-21 13:49:02 +0300

    feat: create func for info-block

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const newToDoForm = document.querySelector(".todo-form");
 const list = document.querySelector(".list");
+const clearCompletedButton = document.querySelector(".clear-all__button");
+const allItemsButton = document.querySelector(".all-items__button");
+const activeItemsButton = document.querySelector(".active-items__button");
+const completedItemsButton = document.querySelector(".completed-items__button");
 
 function addTask(done, text) {
   const li = document.createElement("li");
-  const deleteButton = document.createElement("button");
-  const checkbox = document.createElement("input");
+  li.setAttribute("draggable", "true");
 
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete__button");
   deleteButton.append("x");
   deleteButton.addEventListener("click", handleClickDelete);
 
+  const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
   checkbox.checked = done;
 
@@ -30,3 +36,24 @@ function handleClickDelete(e) {
 }
 
 newToDoForm.addEventListener("submit", handleSubmit);
+
+function handleClearCompleted() {
+  console.log("Clear completed");
+}
+
+function handleAllItemsButton() {
+  console.log("All");
+}
+
+function handleActiveItemsButton() {
+  console.log("Active");
+}
+
+function handleCompletedItemsButton() {
+  console.log("Completed");
+}
+
+clearCompletedButton.addEventListener("click", handleClearCompleted);
+allItemsButton.addEventListener("click", handleAllItemsButton);
+activeItemsButton.addEventListener("click", handleActiveItemsButton);
+completedItemsButton.addEventListener("click", handleCompletedItemsButton);
