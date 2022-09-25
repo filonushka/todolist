ce52bb1892ee7dcd0b5b9fa0c25728aafc96b3b8
filonushka <lero4ka.filonova@mail.ru>
2022-09-25 23:17:05 +0300

    feat: add label, clear-all btn

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const newToDoForm = document.querySelector(".todo-form");
 const list = document.querySelector(".list");
-const clearCompletedButton = document.querySelector(".clear-all__button");
+const clearCompletedButton = document.querySelector(".clear-completed__button");
+const clearAllButton = document.querySelector(".clear-all__button");
 const allItemsButton = document.querySelector(".all-items__button");
 const activeItemsButton = document.querySelector(".active-items__button");
 const completedItemsButton = document.querySelector(".completed-items__button");
+
+let todoArray = [];
+
+clearCompletedButton.addEventListener("click", handleClearCompleted);
+clearAllButton.addEventListener("click", handleClearAll);
+allItemsButton.addEventListener("click", handleAllItemsButton);
+activeItemsButton.addEventListener("click", handleActiveItemsButton);
+completedItemsButton.addEventListener("click", handleCompletedItemsButton);
 
 function addTask(done, text) {
   const li = document.createElement("li");
@@ -16,22 +25,40 @@ function addTask(done, text) {
 
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("id", "task-done");
   checkbox.checked = done;
 
-  li.append(checkbox, text, deleteButton);
+  const label = document.createElement("label");
+  label.setAttribute("id", "task-done");
+  li.append(checkbox, text, deleteButton, label);
   list.appendChild(li);
 }
 
 function handleSubmit(e) {
   const text = newToDoForm.text.value;
   const done = newToDoForm.done.checked;
+  e.preventDefault();
+  if (text === "") {
+    return;
+  }
   addTask(done, text);
   newToDoForm.text.value = "";
-  e.preventDefault();
+
+  const createItemObj = (arr) => {
+    const itemObj = {};
+    itemObj.name = text;
+    itemObj.id = Date.now();
+    itemObj.done = false;
+
+    arr.push(itemObj);
+  };
+  createItemObj(todoArray);
+  localStorage.setItem("key", JSON.stringify(todoArray));
 }
 
 function handleClickDelete(e) {
   const li = e.target.parentElement;
+
   li.remove();
 }
 
@@ -39,6 +66,10 @@ newToDoForm.addEventListener("submit", handleSubmit);
 
 function handleClearCompleted() {
   console.log("Clear completed");
+}
+
+function handleClearAll() {
+  list.innerHTML = "";
 }
 
 function handleAllItemsButton() {
@@ -52,8 +83,3 @@ function handleActiveItemsButton() {
 function handleCompletedItemsButton() {
   console.log("Completed");
 }
-
-clearCompletedButton.addEventListener("click", handleClearCompleted);
-allItemsButton.addEventListener("click", handleAllItemsButton);
-activeItemsButton.addEventListener("click", handleActiveItemsButton);
-completedItemsButton.addEventListener("click", handleCompletedItemsButton);
